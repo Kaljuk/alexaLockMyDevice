4d175fb8ef087939e011cb2f456b5cf78d1f565a
Erik K <erik.kaljumae@gmail.com>
2018-08-05 16:47:55 +0300

    Identify the type of request if possible

--- Alexa/Validation.js
+++ Alexa/Validation.js
@@ -23,16 +23,23 @@ function Validator(o) {
         // Get the application ID from the incoming requests body
         const hasApplicationId = ( req && req.body && req.body.session && req.body.session.application && req.body.session.application.applicationId);
         const applicationId = (hasApplicationId)? req.body.session.application.applicationId : null;
-        // Create an empty body to forward
+        // Create a body with an applicationID to forward
         req.alexa = {
             applicationId
         };
+        
         // Compare the incoming AppId to the valid AppId
         req.alexa.isValid = (applicationId === o.applicationId);
         if (o.log === true)
             log(`Incoming application ID ${isValid?`is`:`isn't`} valid`);
             log(`${applicationId}\nVS\n${o.applicationId}`);
         
+        // Identify the request type
+        const hasRequestType = (req && req.body && req.body.request && req.body.request.type);
+        alexa.requestType = (hasRequestType)? req.body.request.type : null;
+        if (o.log === true)
+            log(`Incoming request, type: ${alexa.requestType}`);
+
         next();
     }
 
